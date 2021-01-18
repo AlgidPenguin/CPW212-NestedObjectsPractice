bd1a5dbd179c7a54e1628b7006763dfcfc17290f
Daniel Ramirez <daniel_r129@hotmail.com>
2021-01-18 00:03:53 -0800

    Added Instructor class

---
+++ NestedObjectsPractice/Instructor.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NestedObjectsPractice
+{
+    class Instructor
+    {
+        /// <summary>
+        /// The legal full name, first and last.
+        /// Example: Danny Brown
+        /// </summary>
+        public string FullName { get; set; }
+
+        /// <summary>
+        /// Company email address.
+        /// Example: dannybrown.cptc.edu
+        /// </summary>
+        public string Email { get; set; }
+    }
+}
